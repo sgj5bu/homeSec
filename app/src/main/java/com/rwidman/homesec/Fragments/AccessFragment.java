ff691e0ca8d639f684e8286ef9e359736ef72a3c
Jonas Ringler <J.Ringler@tekaris.com>
2015-09-23 14:35:44 +0200

    Swype

--- app/src/main/java/com/rwidman/homesec/Fragments/AccessFragment.java
+++ app/src/main/java/com/rwidman/homesec/Fragments/AccessFragment.java
@@ -1,22 +1,14 @@
 package com.rwidman.homesec.Fragments;
 
-import android.app.Activity;
-import android.support.v4.app.ListFragment;
 import android.os.Bundle;
+import android.support.v4.app.ListFragment;
 import android.view.View;
-import android.widget.ListView;
 
 import com.rwidman.homesec.Cache.Cache;
 import com.rwidman.homesec.Fragments.Adapters.AccessAdapter;
 import com.rwidman.homesec.R;
 
-/**
- * A fragment representing a list of Items.
- * <p/>
- * <p/>
- * Activities containing this fragment MUST implement the {@link OnFragmentInteractionListener}
- * interface.
- */
+
 public class AccessFragment extends ListFragment {
 
     // TODO: Rename parameter arguments, choose names that match
@@ -28,7 +20,6 @@ public class AccessFragment extends ListFragment {
     private String mParam1;
     private String mParam2;
 
-    private OnFragmentInteractionListener mListener;
 
     // TODO: Rename and change types of parameters
     public static AccessFragment newInstance(String param1, String param2) {
@@ -60,48 +51,9 @@ public class AccessFragment extends ListFragment {
                 R.layout.accessitem, Cache.getInstance().getAccesses()));
     }
 
-
     @Override
-    public void onAttach(Activity activity) {
-        super.onAttach(activity);
-        try {
-            mListener = (OnFragmentInteractionListener) activity;
-        } catch (ClassCastException e) {
-            throw new ClassCastException(activity.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
+    public void onViewCreated (View view, Bundle savedInstanceState)
+    {
+        setEmptyText("No Access");
     }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    @Override
-    public void onListItemClick(ListView l, View v, int position, long id) {
-        super.onListItemClick(l, v, position, id);
-
-        if (null != mListener) {
-            // Notify the active callbacks interface (the activity, if the
-            // fragment is attached to one) that an item has been selected.
-           // mListener.onFragmentInteraction(Cache.getInstance().getAccesses().get(position).id);
-        }
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        public void onFragmentInteraction(String id);
-    }
-
 }
